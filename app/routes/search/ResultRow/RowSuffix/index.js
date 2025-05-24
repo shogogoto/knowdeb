2836453fe1611d7f79f2fafa698e4c842c8b5c99
Shogo Goto <gotoadmn0605@gmail.com>
2025-05-24 19:00:29 +0900

    よく分からん生成されたファイル削除したらnpm run dev 動いた

--- app/routes/search/ResultRow/RowSuffix/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = RowSuffix;
-function RowSuffix(_a) {
-    var knowde = _a.knowde;
-    return (<>
-      <div className="border">{knowde.when}</div>
-    </>);
-}
