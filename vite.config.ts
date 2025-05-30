9a8888ae61f9c56276a1876ddd3ec830b3e3eb98
Shogo Goto <gotoadmn0605@gmail.com>
2025-05-30 13:49:58 +0900

    add mdx config for documents

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
+import mdx from "@mdx-js/rollup";
 import netlifyPlugin from "@netlify/vite-plugin-react-router";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import reactVitest from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-
 export default defineConfig({
   plugins: [
     tailwindcss(),
     process.env.VITEST ? reactVitest() : reactRouter(), // storybookのテスト解消
     tsconfigPaths(),
     netlifyPlugin(),
+    {
+      enforce: "pre",
+      ...mdx(),
+    },
   ],
   test: {
     globals: true, // テスト関数のインポート省略
