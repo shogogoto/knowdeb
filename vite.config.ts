0b9827f134dfecb5ce203937416d32964e4067db
Shogo Goto <gotoadmn0605@gmail.com>
2025-06-05 15:01:31 +0900

    mdxにmermaidを埋め込めるようにした

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,29 @@ import netlifyPlugin from "@netlify/vite-plugin-react-router";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import reactVitest from "@vitejs/plugin-react";
+import rehypeMermaid from "rehype-mermaid";
+import remarkGfm from "remark-gfm";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
     process.env.VITEST ? reactVitest() : reactRouter(), // storybookのテスト解消
     tsconfigPaths(),
     netlifyPlugin(),
-    { enforce: "pre", ...mdx() },
+    {
+      enforce: "pre",
+      ...mdx({
+        remarkPlugins: [
+          remarkGfm,
+          // remark-mermaidjs は不要になる
+        ],
+        rehypePlugins: [
+          [rehypeMermaid, { strategy: "img-svg" }], // strategy オプションは重要
+        ],
+      }),
+    },
   ],
   test: {
     globals: true, // テスト関数のインポート省略
