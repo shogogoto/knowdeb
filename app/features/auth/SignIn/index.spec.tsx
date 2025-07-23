9ac8d8700a1e80c1338493c05d3cd53a58583a83
Shogo Goto <gotoadmn0605@gmail.com>
2025-07-23 17:18:53 +0900

    test

--- app/features/auth/SignIn/index.spec.tsx
+++ app/features/auth/SignIn/index.spec.tsx
@@ -1,23 +1,51 @@
-import { createRoutesStub } from "react-router";
+import { render, screen } from "@testing-library/react";
+import { setupServer } from "msw/node";
+import { RouterProvider, createMemoryRouter } from "react-router";
+import { getAuthMock } from "~/generated/auth/auth.msw";
+import { getGoogleMock } from "~/generated/google/google.msw";
+import { getUserMock } from "~/generated/user/user.msw";
 import SignInForm from ".";
+import { AuthProvider } from "../AuthProvider";
+import * as AuthMock from "../AuthProvider";
 
-//
+const server = setupServer(
+  ...getGoogleMock(),
+  ...getUserMock(),
+  ...getAuthMock(),
+);
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+const routesFixture = [
+  {
+    path: "/login",
+    // Component: () => <Navigate to="/home" />,
+    Component: () => (
+      <AuthProvider>
+        <SignInForm />
+      </AuthProvider>
+    ),
+  },
+  {
+    path: "/home",
+    Component: () => <div>home</div>,
+  },
+  {
+    path: "/",
+    Component: () => <div>root</div>,
+  },
+];
+
 describe("ログイン", () => {
-  it("Google ログイン 成功", async () => {
-    const Stub = createRoutesStub([
-      {
-        path: "/login",
-        Component: SignInForm,
-      },
-    ]);
-
-    expect(true).toBe(true);
-  });
-
-  it("Google ログイン 失敗", async () => {
-    expect(true).toBe(true);
-  });
-  it("ログイン済みだったらhomeへリダイレクト", async () => {
-    expect(true).toBe(true);
+  it("ログイン済みはhomeへリダイレクト", async () => {
+    // @ts-ignore
+    vi.spyOn(AuthMock, "useAuth").mockReturnValue({ isAuthorized: true });
+    const router = createMemoryRouter(routesFixture, {
+      initialEntries: ["/login"],
+    });
+    render(<RouterProvider router={router} />);
+    await screen.findByText("home");
+    // expect(router.state.location.pathname).toBe("/home");
   });
 });
