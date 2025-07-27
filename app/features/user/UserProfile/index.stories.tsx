96b7177a06b511c0b28cbb5194d3cbd7f987c170
Shogo Goto <gotoadmn0605@gmail.com>
2025-07-27 18:34:42 +0900

    プロフィール画像を含める

--- app/features/user/UserProfile/index.stories.tsx
+++ app/features/user/UserProfile/index.stories.tsx
@@ -1,25 +1,36 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
-
-import { faker } from "@faker-js/faker";
+import { AuthProvider } from "~/features/auth/AuthProvider";
+import type { UserRead } from "~/generated/fastAPI.schemas";
+import { getUsersCurrentUserUserMeGetMockHandler } from "~/generated/user/user.msw";
 import Index from "./index";
 
 const meta = {
   component: Index,
+  decorators: [
+    (Story) => (
+      <AuthProvider>
+        <Story />
+      </AuthProvider>
+    ),
+  ],
+  parameters: {
+    msw: {
+      handlers: [
+        getUsersCurrentUserUserMeGetMockHandler({
+          display_name: "ナナシ",
+          profile: "profile",
+          username: "0123456789",
+          uid: "382d74c3-5782-4930-aaa1-ef3ea08f506a",
+          created: "2023-01-01",
+          avatar_url: "https://github.com/shadcn.png",
+        } as UserRead),
+      ],
+    },
+  },
 } satisfies Meta<typeof Index>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  args: {
-    user: {
-      display_name: "ナナシ",
-      profile: faker.helpers.arrayElement([faker.string.alpha(160), null]),
-      username: "0123456789",
-      uid: "382d74c3-5782-4930-aaa1-ef3ea08f506a",
-      created: "2023-01-01",
-      avatar_url: "https://github.com/shadcn.png",
-    },
-  },
-};
+export const Default: Story = {};
--- app/features/user/UserProfile/index.tsx
+++ app/features/user/UserProfile/index.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router";
+import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
 import { Button } from "~/components/ui/button";
 import { useAuth } from "~/features/auth/AuthProvider";
 
@@ -6,36 +7,33 @@ export default function UserProfile() {
   const { user } = useAuth();
 
   return (
-    <div className="space-y-6 bg-white dark:bg-gray-800 p-6 shadow-md">
-      <div className="flex items-center space-x-4 pt-4">
-        <div className="flex-1">
-          <h2 className="text-2xl font-bold text-gray-800 dark:text-gray-100 break-words">
-            {user?.display_name || "名無しさん"}
-          </h2>
-          <p className="text-gray-600 dark:text-gray-300 text-sm break-words">
-            {`@${user?.username || user?.uid || "userId"}`}
-          </p>
-        </div>
+    <div className="space-y-4 bg-white dark:bg-gray-800 p-6 shadow-md">
+      <div className="flex justify-between items-start">
+        <Avatar className="h-24 w-24">
+          <AvatarImage
+            src={user?.avatar_url ?? undefined}
+            alt={user?.display_name ?? undefined}
+          />
+          <AvatarFallback>
+            {user?.display_name?.charAt(0) || "N"}
+          </AvatarFallback>
+        </Avatar>
         <Button asChild className="px-4 py-2 text-md">
           <Link to="/user/edit">編集</Link>
         </Button>
       </div>
-      <div className="text-gray-700 dark:text-gray-200 text-base mt-4 p-3 bg-gray-100 dark:bg-gray-700 rounded-md overflow-hidden break-words whitespace-pre-line">
-        {user?.profile
-          ? user.profile.length > 160
-            ? `${user.profile.substring(0, 160)}...`
-            : user.profile
-          : "プロフィールが設定されていません。"}
+
+      <div>
+        <h2 className="text-2xl font-bold text-gray-800 dark:text-gray-100 break-words">
+          {user?.display_name || "名無しさん"}
+        </h2>
+        <p className="text-gray-600 dark:text-gray-300 text-sm break-words">
+          {`@${user?.username || user?.uid || "userId"}`}
+        </p>
       </div>
 
-      <div className="mt-6 pt-6 border-t border-gray-200 dark:border-gray-700 text-sm text-gray-500 dark:text-gray-400 space-y-2">
-        <h3 className="font-semibold">デバッグ情報:</h3>
-        <p>ID: {user?.uid as string}</p>
-        <p>Email: {user?.email as string}</p>
-        <p>Username: {user?.username as string}</p>
-        <p>Display Name: {user?.display_name}</p>
-        <p>Profile: {user?.profile}</p>
-        <p>Avatar URL: {user?.avatar_url}</p>
+      <div className="text-gray-700 dark:text-gray-200 text-base pt-4 overflow-hidden break-words whitespace-pre-line">
+        {user?.profile || "プロフィールが設定されていません。"}
       </div>
     </div>
   );
