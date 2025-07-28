ed590b8595969a87e171cda21a9ed350168491b2
Shogo Goto <gotoadmn0605@gmail.com>
2025-07-28 14:42:35 +0900

    スマホで表示されなかったfavicon設定し直し

--- app/root.tsx
+++ app/root.tsx
@@ -22,10 +22,10 @@ export const links: Route.LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
-  { rel: "icon", href: "/favicon.ico?v=1", sizes: "any" },
-  { rel: "icon", href: "/favicon.svg?v=1", type: "image/svg+xml" },
-  { rel: "apple-touch-icon", href: "/apple-touch-icon.png?v=1" },
-  { rel: "manifest", href: "/manifest.json?v=1" },
+  { rel: "icon", href: "/favicon.ico", sizes: "32x32" },
+  { rel: "icon", href: "/favicon.svg", type: "image/svg+xml", sizes: "any" },
+  { rel: "apple-touch-icon", href: "/apple-touch-icon.png" },
+  { rel: "manifest", href: "/manifest.json" },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
