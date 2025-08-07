bd94be3ed7e0026cfee5389d6cbaf2613bc4210f
Shogo Goto <gotoadmn0605@gmail.com>
2025-08-07 21:13:57 +0900

    fix story

--- app/components/Pagenation/replace.stories.tsx
+++ app/components/Pagenation/replace.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
 
-import Replace from "./replace";
+import Replace, { type PaginationProps } from "./replace";
 
 const meta = {
   component: Replace,
@@ -10,42 +10,54 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
+type Props = {
+  n_page: number;
+  current?: number;
+};
+
+function fixProps(props: Props): PaginationProps {
+  const { n_page, current } = props;
+  const pageSize = 10;
+  const total = n_page * pageSize;
+  return { total, pageSize, initial: current };
+}
+
 export const Default: Story = {
-  args: { n_page: 20 },
+  args: fixProps({ n_page: 20 }),
 };
 
 export const Zero: Story = {
-  args: { n_page: 0 },
+  args: fixProps({ n_page: 0 }),
 };
 
 export const One: Story = {
-  args: { n_page: 1 },
+  args: fixProps({ n_page: 1 }),
 };
 
 export const Two: Story = {
-  args: { n_page: 2 },
+  args: fixProps({ n_page: 2 }),
 };
 
 export const Five: Story = {
-  args: { n_page: 5 },
+  args: fixProps({ n_page: 5 }),
 };
 
 export const Six: Story = {
-  args: { n_page: 6 },
+  args: fixProps({ n_page: 6 }),
 };
 
 export const Ten: Story = {
-  args: { n_page: 10 },
+  args: fixProps({ n_page: 10 }),
 };
 
 export const Fifteen: Story = {
-  args: { n_page: 15, current: 10 },
+  args: fixProps({ n_page: 15, current: 10 }),
 };
 
 export const FifteenFirst: Story = {
-  args: { n_page: 15, current: 1 },
+  args: fixProps({ n_page: 15, current: 1 }),
 };
 
 export const FifteenLast: Story = {
-  args: { n_page: 15, current: 15 },
+  args: fixProps({ n_page: 15, current: 15 }),
 };
