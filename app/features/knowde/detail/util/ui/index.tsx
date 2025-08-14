d97acdae4d679d33e27d5179d15029637095bb64
Shogo Goto <gotoadmn0605@gmail.com>
2025-08-14 19:37:07 +0900

    次はWebGL2RenderingContext is not defined だってさ。sigma.jsを削除

--- app/features/knowde/detail/util/ui/index.tsx
+++ app/features/knowde/detail/util/ui/index.tsx
@@ -1,12 +1,5 @@
 import "@react-sigma/core/lib/style.css";
 import "@react-sigma/graph-search/lib/style.css";
-import {
-  ControlsContainer,
-  FullScreenControl,
-  SigmaContainer,
-  ZoomControl,
-} from "@react-sigma/core";
-import { MiniMap } from "@react-sigma/minimap";
 import type { KnowdeDetail } from "~/generated/fastAPI.schemas";
 import { toDisplayGraph } from "..";
 
@@ -29,32 +22,33 @@ export default function DisplayGraph({ detail }: Props) {
   const { graph } = toDisplayGraph(detail);
 
   return (
-    <SigmaContainer
-      graph={graph}
-      //style={{ height: "100vh", width: "100%" }}
-      className="border"
-      settings={{
-        renderEdgeLabels: true,
-        defaultEdgeType: "arrow",
-        allowInvalidContainer: true,
-      }}
-    >
-      {/* <Fa2 /> */}
-      <ControlsContainer
-        position={"bottom-right"}
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <ZoomControl />
-        <FullScreenControl />
-        {/* <LayoutForceAtlas2Control /> */}
-      </ControlsContainer>
-      <ControlsContainer position={"bottom-left"}>
-        <MiniMap width="100px" height="100px" />
-      </ControlsContainer>
-    </SigmaContainer>
+    <div>aaaa</div>
+    // <SigmaContainer
+    //   graph={graph}
+    //   //style={{ height: "100vh", width: "100%" }}
+    //   className="border"
+    //   settings={{
+    //     renderEdgeLabels: true,
+    //     defaultEdgeType: "arrow",
+    //     allowInvalidContainer: true,
+    //   }}
+    // >
+    //   {/* <Fa2 /> */}
+    //   <ControlsContainer
+    //     position={"bottom-right"}
+    //     style={{
+    //       display: "flex",
+    //       flexDirection: "column",
+    //       alignItems: "center",
+    //     }}
+    //   >
+    //     <ZoomControl />
+    //     <FullScreenControl />
+    //     {/* <LayoutForceAtlas2Control /> */}
+    //   </ControlsContainer>
+    //   <ControlsContainer position={"bottom-left"}>
+    //     <MiniMap width="100px" height="100px" />
+    //   </ControlsContainer>
+    // </SigmaContainer>
   );
 }
