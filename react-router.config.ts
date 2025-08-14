f9d84adae87bc9f87837a72138200b774102a86c
Shogo Goto <gotoadmn0605@gmail.com>
2025-08-14 22:17:42 +0900

    あれ、prerender いける？

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,14 +2,14 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   ssr: true,
-  // async prerender() {
-  //   return [
-  //     "/",
-  //     "/docs/toc",
-  //     "/docs/get-started",
-  //     "/docs/concept",
-  //     "/docs/cli",
-  //     "/docs/features",
-  //   ];
-  // },
+  async prerender() {
+    return [
+      "/",
+      "/docs/toc",
+      "/docs/get-started",
+      "/docs/concept",
+      "/docs/cli",
+      "/docs/features",
+    ];
+  },
 } satisfies Config;
