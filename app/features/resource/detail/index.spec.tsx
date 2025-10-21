f11b4225b588c969a696cf21eb20afb9441378b2
Shogo Goto <gotoadmn0605@gmail.com>
2025-10-21 20:52:04 +0900

    graphを辿るutil

--- app/features/resource/detail/index.spec.tsx
+++ app/features/resource/detail/index.spec.tsx
@@ -1,13 +1,33 @@
+import { toGraph } from "~/shared/lib/network";
+import { resourceDetailFiture } from "./fixture";
+import { toAdjacent, useTraceMemory } from "./util";
+
+const { g, resource_info, uids, terms } = resourceDetailFiture;
+const G = toGraph(g);
+
 describe("ResourceDetail", () => {
-  it("get head tree", () => {
-    // const { g, resource_info, uids, terms } = resourceDetailFiture;
-    // const { user, resource, resource_stats } = resource_info;
-    //
-    // const root = resource.uid;
-    // const graph = toGraph(g);
-    //
-    // console.log(JSON.stringify({ tree: toTree(graph, root) }));
-    //
-    // expect(true).toBe(false);
+  it("関係の周辺", () => {
+    const id = "e4254a62-74cd-46d2-9d75-2e69a717c2ec";
+    const adj = toAdjacent(id, G, uids, terms);
+    expect(adj.kn.term?.names?.[0]).toBe("デイヴィッド・チャーマーズ");
+    expect(adj.kn.sentence).toBe("28歳でクオリアが原理的に解明されない");
+    expect(adj.premises()).toHaveLength(0);
+    expect(adj.siblings()).toHaveLength(0); // 他の関係は省略
+  });
+
+  it("downArrays", () => {
+    const rootId = resource_info.resource.uid;
+    const adj = toAdjacent(rootId, G, uids, terms);
+    for (const downarr of adj.downArrays()) {
+      expect(downarr).toHaveLength(3);
+    }
+  });
+
+  it("辿ったことを記憶", () => {
+    const { register, isRegistered } = useTraceMemory();
+    const id = "e4254a62-74cd-46d2-9d75-2e69a717c2ec";
+    expect(isRegistered(id)).toBe(false);
+    register(id);
+    expect(isRegistered(id)).toBe(true);
   });
 });
--- app/features/resource/detail/util.ts
+++ app/features/resource/detail/util.ts
@@ -1,5 +1,11 @@
 import type Graph from "graphology";
-import { eqEdgeType, pathsToEnd, succ } from "~/shared/lib/network";
+import type {
+  EdgeType,
+  Knowde,
+  ResourceDetailTerms,
+  ResourceDetailUids,
+} from "~/shared/generated/fastAPI.schemas";
+import { eqEdgeType, pathsToEnd, pred, succ } from "~/shared/lib/network";
 
 export function toTree(g: Graph, root: string) {
   const tops = succ(g, root, eqEdgeType("below"));
@@ -15,4 +21,92 @@ export function toTree(g: Graph, root: string) {
       // [head]: ,
     };
   });
+}
+
+type ResourceKnowde = Omit<Knowde, "stats" | "resource_uid">;
+
+export function toAdjacent(
+  id: string,
+  g: Graph,
+  uids: ResourceDetailUids,
+  terms: ResourceDetailTerms,
+) {
+  // @ts-ignore
+  const when = succ(g, id, eqEdgeType("when")).map((wid) => uids[wid])?.[0]?.n;
+  const additional = when && { when };
+  const kn: ResourceKnowde = {
+    sentence: (uids[id] as string) ?? "<<<not defined>>>",
+    term: terms[id],
+    uid: id,
+    additional,
+  };
+
+  function toSucc(et: EdgeType) {
+    return succ(g, id, eqEdgeType(et)).map((sid) =>
+      toAdjacent(sid, g, uids, terms),
+    );
+  }
+
+  function toPred(et: EdgeType) {
+    return pred(g, id, eqEdgeType(et)).map((sid) =>
+      toAdjacent(sid, g, uids, terms),
+    );
+  }
+
+  function toEnd(et: EdgeType) {
+    return pathsToEnd(g, id, eqEdgeType(et), succ).map((path) => {
+      return path.map((sid) => toAdjacent(sid, g, uids, terms));
+    });
+  }
+
+  // 1つ下の階層の一覧を返す
+  function downArrays() {
+    const retval: ResourceKnowde[][] = [];
+    for (const below of toSucc("below")) {
+      const sibs = below.toEnd("sibling")[0]?.map((e) => e.kn);
+      sibs && retval.push(sibs);
+    }
+    return retval;
+  }
+
+  return {
+    kn,
+    toSucc,
+    toPred,
+    toEnd,
+    siblings: () => toSucc("sibling"),
+    belows: () => toSucc("below"),
+    downArrays,
+    conclusions: () => toSucc("to"),
+    premises: () => toPred("to"),
+    refers: () => toSucc("resolved"),
+    referreds: () => toPred("resolved"),
+  };
+}
+
+// below と sibling を 再帰的に返す
+//  まずは headerのツリーを作成したい
+export function toDetailTree() {}
+
+// 再帰的に周辺を辿ると無限ループになるので
+// 一度辿ったものか否かを判定する
+export function useTraceMemory() {
+  const tracememory = new Set<string>();
+
+  function register(id: string) {
+    tracememory.add(id);
+  }
+
+  function isRegistered(id: string) {
+    return tracememory.has(id);
+  }
+
+  return {
+    register,
+    isRegistered,
+  };
+}
+
+export function isHeading(id: string, uids: ResourceDetailUids) {
+  return uids[id]?.toString().startsWith("#");
 }
