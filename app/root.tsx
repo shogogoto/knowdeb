b18faa56d24770b6482ca6a8d9876b3a7e59528a
Shogo Goto <gotoadmn0605@gmail.com>
2025-12-03 18:46:22 +0900

    fix

--- app/root.tsx
+++ app/root.tsx
@@ -69,8 +69,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             src="https://www.googletagmanager.com/ns.html?id=GTM-PKBJMGBW"
             height="0"
             width="0"
-            // @ts-ignore
-            style="display:none;visibility:hidden"
+            style={{ display: "none", visibility: "hidden" }}
           />
         </noscript>
 
