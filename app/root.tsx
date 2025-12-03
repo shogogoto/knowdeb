f4e9ed970e0e36efec7656b5e7c92db4f83b8d41
Shogo Goto <gotoadmn0605@gmail.com>
2025-12-03 18:28:26 +0900

    google tag manager

--- app/root.tsx
+++ app/root.tsx
@@ -42,6 +42,16 @@ export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ja">
       <head>
+        <script
+          // biome-ignore lint/security/noDangerouslySetInnerHtml:
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PKBJMGBW');`,
+          }}
+        />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta
@@ -53,6 +63,17 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <ThemeScript />
       </head>
       <body>
+        <noscript>
+          <iframe
+            title="google tag manager"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PKBJMGBW"
+            height="0"
+            width="0"
+            // @ts-ignore
+            style="display:none;visibility:hidden"
+          />
+        </noscript>
+
         {children}
         <ScrollRestoration />
         <Scripts />
