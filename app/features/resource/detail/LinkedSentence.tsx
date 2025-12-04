50de19d31290d4b4bcdac711d01509807082f3a5
Shogo Goto <gotoadmn0605@gmail.com>
2025-12-04 18:32:31 +0900

    数式をResourceViewで表示、連番は削除

--- app/features/resource/detail/LinkedSentence.tsx
+++ app/features/resource/detail/LinkedSentence.tsx
@@ -1,44 +1,25 @@
+import type { PropsWithChildren } from "react";
 import { Link } from "react-router";
 import { HashLink } from "react-router-hash-link";
-import type {
-  MResource,
-  UserReadPublic,
-} from "~/shared/generated/fastAPI.schemas";
 import { useSelectPreventLink } from "~/shared/hooks/useSelectPrevent";
 import { useResourceDetail } from "./Context";
 import type { toAdjacent } from "./util";
 
-type Props = {
-  adj: ReturnType<typeof toAdjacent>;
-  resource?: MResource;
-  user?: UserReadPublic;
-};
-
 const PATTERN = /(\{[^}]*\})/g;
 
-export default function LinkedSentence({ adj }: Props) {
-  const { resource_info } = useResourceDetail();
-  const { handleMouseDown, handleClick } = useSelectPreventLink(5); // 5px を閾値とする
-  const { rootId } = useResourceDetail();
-  const refs = adj.refers();
+type Props = {
+  sentence: string;
+  refers: ReturnType<ReturnType<typeof toAdjacent>["refers"]>;
+};
 
-  const parts = adj.kn.sentence.split(PATTERN).filter(Boolean);
+// 用語リンクを埋め込んだ単文
+export default function RefLinkSentence({ sentence, refers }: Props) {
+  const { rootId } = useResourceDetail();
+  const parts = sentence.split(PATTERN).filter(Boolean);
   return (
-    <Link
-      to={`/knowde/${adj.kn.uid}`}
-      draggable="false"
-      className="!text-inherit"
-      onMouseDown={handleMouseDown}
-      onClick={handleClick}
-      id={adj.kn.uid}
-      state={{
-        knowde: adj.kn,
-        resource: resource_info.resource,
-        user: resource_info.user,
-      }}
-    >
+    <>
       {parts.map((part) => {
-        const found = refs.find((r) =>
+        const found = refers.find((r) =>
           r.kn.term?.names?.some((n) => part.includes(n)),
         );
 
@@ -51,11 +32,32 @@ export default function LinkedSentence({ adj }: Props) {
         }
         return <span key={part}>{part}</span>; // part;
       })}
-    </Link>
+    </>
   );
 }
 
-// function extractBraces(str: string): string[] {
-//   const matches = Array.from(str.matchAll(PATTERN));
-//   return matches.map((match) => match[1]);
-// }
+type Props2 = {
+  kn: ReturnType<typeof toAdjacent>["kn"];
+} & PropsWithChildren;
+
+export function KnowdeDetailLink({ kn, children }: Props2) {
+  const { resource_info } = useResourceDetail();
+  const { handleMouseDown, handleClick } = useSelectPreventLink(5); // 5px を閾値とする
+  return (
+    <Link
+      to={`/knowde/${kn.uid}`}
+      draggable="false"
+      className="!text-inherit"
+      onMouseDown={handleMouseDown}
+      onClick={handleClick}
+      id={kn.uid}
+      state={{
+        knowde: kn,
+        resource: resource_info.resource,
+        user: resource_info.user,
+      }}
+    >
+      {children}
+    </Link>
+  );
+}
